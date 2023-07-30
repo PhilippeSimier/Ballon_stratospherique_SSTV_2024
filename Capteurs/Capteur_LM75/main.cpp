b275992d3fa3969b5f25d5ed155966675bb48112
Philippe SIMIER <philaure@wanadoo.fr>
2023-07-30 08:57:41 +0200

    add retour à la ligne
    et test avec netbeans sous windows

--- Capteurs/Capteur_LM75/main.cpp
+++ Capteurs/Capteur_LM75/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv) {
         LM75 capteur;
 
         while (1) {
-            cout << setprecision(3) << capteur.getTemperature() << endl;
+            cout << setprecision(3) << capteur.getTemperature() << '\r' <<endl;
             sleep(1);
         }
     } catch (const runtime_error &e) {
