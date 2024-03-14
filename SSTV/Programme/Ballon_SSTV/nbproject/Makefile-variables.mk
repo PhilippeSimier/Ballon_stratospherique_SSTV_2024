52ee80ad4f99e8111322c6d9050ea3ef493de17b
philippeSimier <philaure@wanadoo.fr>
2024-03-14 17:58:34 +0100

    add prog SSTV

---
+++ SSTV/Programme/Ballon_SSTV/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=ballon_sstv
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/ballon_sstv
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=ballonsstv.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/ballonsstv.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=ballon_sstv
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/ballon_sstv
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=ballonsstv.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/ballonsstv.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
