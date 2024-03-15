8b8d07eba37cccd08df09810180a06318b36e1f5
philippeSimier <philaure@wanadoo.fr>
2024-03-15 15:56:00 +0100

    improve

--- SSTV/Programme/Ballon_SSTV/main.cpp
+++ SSTV/Programme/Ballon_SSTV/main.cpp
@@ -13,19 +13,8 @@
 using namespace std;
 
 // Fonction à exécuter de manière synchronisée
+void envoyerPhoto();
 
-void envoyerPhoto() {
-
-    std::cout << "Envoi d'une photo." << std::endl;
-    // Prise d'une photo en basse résolution
-    system("libcamera-still --width 320 --height 256 -o /home/ballon/photo.jpg");
-    // add texte
-    system("convert -pointsize 20 -fill black -draw \"text 10,20 'F4KMN `date`'\" /home/ballon/photo.jpg /home/ballon/photo_date.jpg");
-    // Convertion en RGB 8 bits
-    system("convert -depth 8 /home/ballon/photo_date.jpg /home/ballon/photo_date.rgb");
-    // Emission SSTV
-    system("sudo /home/ballon/rpitx/pisstv /home/ballon/photo_date.rgb 28680000");
-}
 
 int main(int argc, char** argv) {
 
@@ -36,8 +25,9 @@ int main(int argc, char** argv) {
         auto tempsActuel = chrono::system_clock::to_time_t(maintenant);
         auto tmMaintenant = *localtime(&tempsActuel);
 
-        // Vérifier si c'est la seconde 0 et que c'est un multiple de 5 minutes
-        if (tmMaintenant.tm_sec == 0 && tmMaintenant.tm_min % 5 == 0) {
+        // Vérifier si c'est un multiple de 5 minutes - 6s
+        if (tmMaintenant.tm_sec == 54 && tmMaintenant.tm_min % 5 == 4) {
+            
             // Appeler la fonction synchronisée
             envoyerPhoto();
 
@@ -51,5 +41,18 @@ int main(int argc, char** argv) {
     }
 
     return 0;
+}
+
+void envoyerPhoto() {
+
+    std::cout << "Envoi d'une photo." << std::endl;
+    // Prise d'une photo en basse résolution
+    system("libcamera-still --width 320 --height 256 -o /home/ballon/photo.jpg");
+    // add texte
+    system("convert -pointsize 20 -fill black -draw \"text 10,20 'F4KMN `date +\"%D %T\"`'\" /home/ballon/photo.jpg /home/ballon/photo_date.jpg");
+    // Convertion en RGB 8 bits
+    system("convert -depth 8 /home/ballon/photo_date.jpg /home/ballon/photo_date.rgb");
+    // Emission SSTV
+    system("sudo /home/ballon/rpitx/pisstv /home/ballon/photo_date.rgb 28680000");
 }
 
