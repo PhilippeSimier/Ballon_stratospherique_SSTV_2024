a26fb1e0350d8bda929f81c1dde4b61476ad7bc0
philippeSimier <philaure@wanadoo.fr>
2024-03-21 08:31:03 +0100

    test étudiants

---
+++ SSTV/Programme/test/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: psimier
+ *
+ * Created on 21 mars 2024, 08:21
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+    
+    cout << "programme test \n\r" << endl;
+
+    return 0;
+}
+
