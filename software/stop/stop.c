2b014ab556415c0500e96052cdfd62a7682d29cd
philippeSimier <philaure@wanadoo.fr>
2024-05-24 14:51:13 +0200

    add processus stop

---
+++ software/stop/stop.c
@@ -0,0 +1,45 @@
+/*
+ * File:   stop.p
+ * Author: philippe SIMIER
+ *
+ * Created on 24 mai 2024, 14:13
+ * Installer git clone https://github.com/WiringPi/WiringPi.git
+ *           cd WiringPi
+ *           ./build
+ * ajouter l'option de compilation -l wiringPi lors de la compilation
+ *
+ * gcc ./stop.c -o stop -l wiringPi
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <wiringPi.h> // Include WiringPi library!
+
+int main(void)
+{
+
+  wiringPiSetupGpio();
+
+  // pin mode ..(INPUT, OUTPUT, PWM_OUTPUT, GPIO_CLOCK)
+  // set pin 25 to input
+  pinMode(25, INPUT);
+
+  // pull up/down mode (PUD_OFF, PUD_UP, PUD_DOWN) => down
+  pullUpDnControl(25, PUD_OFF);
+
+  while(1) {
+      // get state of pin 17
+      int value = digitalRead(25);
+
+      if (value == LOW)
+      {
+          printf("bouton pousoir actionné\r\n");
+          system("halt -p");
+      }
+
+      sleep(1);
+  }
+}
