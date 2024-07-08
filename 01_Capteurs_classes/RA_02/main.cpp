97676a0c6c76445f1ca12c4c61d5b290552dd88c
philippeSimier <philaure@wanadoo.fr>
2024-07-08 23:48:08 +0200

    add classe SX1578

--- 01_Capteurs_classes/RA_02/main.cpp
+++ 01_Capteurs_classes/RA_02/main.cpp
@@ -2,7 +2,7 @@
  * File:   main.cpp
  * Author: philippe SIMIER
  * 
- * Programme test unitaire classe Spi
+ * Programme test unitaire classe SX1278
  * test les 4 méthodes 
  *
  * Created on 7 juillet 2024, 16:14
@@ -12,12 +12,7 @@
 #include <iostream>
 #include "SX1278.h"
 
-#define REG_FIFO 0x00
-#define REG_FIFO_TX_BASE_ADDR 0x0E
-#define REG_FIFO_RX_BASE_ADDR 0x0F
-#define TX_BASE_ADDR 0x00
-#define RX_BASE_ADDR 0x00
-#define REG_PAYLOAD_LENGTH 0x22
+
 
 using namespace std;
 
@@ -32,17 +27,15 @@ int main(int argc, char** argv) {
         SX1278 loRa;  // channel SPI 0
         
         loRa.begin(433775000);
-        
-        
+     
+       
         loRa.send(message, 6);
         sleep(3);
         loRa.send(message, 6);
         sleep(3);
         loRa.send(message, 6);
         sleep(3);
-        
-
-
+      
        
     } catch (const std::runtime_error &e) {
         
