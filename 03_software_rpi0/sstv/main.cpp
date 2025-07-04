97afa88be0fba49558a72fd725d3ac56b0ba12b5
philippeSimier <philaure@wanadoo.fr>
2025-07-04 22:27:06 +0200

    improve sstv

--- 03_software_rpi0/sstv/main.cpp
+++ 03_software_rpi0/sstv/main.cpp
@@ -20,8 +20,6 @@
 
 using namespace std;
 
-//   Fonction exécutée par le deuxième thread
-void threadFunction1();
 
 
 GpioOut ptt(6);  // Commande Push To Talk
@@ -31,7 +29,6 @@ int main(int argc, char** argv) {
 
     Camera camera;
 
-    thread t1(threadFunction1);
     while (true) {
 
         // Obtenir l'heure actuelle
@@ -46,31 +43,6 @@ int main(int argc, char** argv) {
             this_thread::sleep_for(chrono::seconds(1));
 
         }
-    }
-    // Attente de la fin du thread t1
-    t1.join();
-    return 0;
-
-}
-
-
-
-
-/**
- *    Fonction exécutée par le deuxième thread
- */
-void threadFunction1() {
-
-    extern GpioOut ptt;
-
-    while (true) {
-
-        // Obtenir l'heure actuelle
-        auto maintenant = chrono::system_clock::now();
-        auto tempsActuel = chrono::system_clock::to_time_t(maintenant);
-        auto tmMaintenant = *localtime(&tempsActuel);
-
-        // Vérifier si c'est un multiple de 5 minutes - 6s
         if (tmMaintenant.tm_sec == 54 && tmMaintenant.tm_min % 5 == 4) {
 
             // Appeler la methode envoyer photo en SSTV
@@ -80,6 +52,16 @@ void threadFunction1() {
             ptt.setOff(); // désactive l'ampli
 
         }
+
+
     }
+
+    return 0;
+
 }
+
+
+
+
+
 
