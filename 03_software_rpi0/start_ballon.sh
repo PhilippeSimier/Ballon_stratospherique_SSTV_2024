a1ae22a2e7c2991b4bf7f17e3b43374a2a242b0b
philippeSimier <philaure@wanadoo.fr>
2025-07-05 10:04:08 +0200

    fichier CSV dans répertoire data

--- 03_software_rpi0/start_ballon.sh
+++ 03_software_rpi0/start_ballon.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-sleep 5
-/home/ballon/stop/stop &
+sleep 30
+/home/ballon/stop/stop >> /home/ballon/log/stop.log 2>&1 &
 sleep 1
-/home/ballon/sstv/sstv &
-sleep 1
-/home/ballon/Telemetrie/Telemetrie &
-sleep 1
-/home/ballon/Reception/reception &
-sleep 1
-/home/ballon/lora_files/lora_files >> /home/ballon/log.txt 2>&1 &
-sleep 1
-/home/ballon/lora_files/lora_files >> /home/ballon/log.txt 2>&1 &
+/home/ballon/lora_files/lora_files >> /home/ballon/log/lora.log 2>&1 &
+/home/ballon/telemetrie/telemetrie >> /home/ballon/log/telemetrie.log 2>&1 &
+/home/ballon/sstv/sstv >> /home/ballon/log/sstv.log 2>&1 &
