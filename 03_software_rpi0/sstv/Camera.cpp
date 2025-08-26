58de6cac907c807a6f2c79022086b103e8787c25
philippeSimier <philaure@wanadoo.fr>
2025-08-26 15:57:52 +0200

    camera

--- 03_software_rpi0/sstv/Camera.cpp
+++ 03_software_rpi0/sstv/Camera.cpp
@@ -51,7 +51,7 @@ void Camera::envoyerPhoto(){
 
     // Emission SSTV
     commande.str(""); commande.clear();
-    commande << "/home/ballon/rpitx/pisstv/ramfs/photo_date.rgb ";
+    commande << "/home/ballon/rpitx/pisstv /ramfs/photo_date.rgb ";
     commande << frequence;
     system(commande.str().c_str());
 }
