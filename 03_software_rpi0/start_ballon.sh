602623343b23f7c49cc93dc5a5af5fbb55af6e08
philippeSimier <philaure@wanadoo.fr>
2025-08-26 15:11:10 +0200

    update Guidance et start_ballon.sh

--- 03_software_rpi0/start_ballon.sh
+++ 03_software_rpi0/start_ballon.sh
@@ -4,4 +4,5 @@ sleep 30
 sleep 1
 /home/ballon/lora_files/lora_files >> /home/ballon/log/lora.log 2>&1 &
 /home/ballon/weather/weather >> /home/ballon/log/weather.log 2>&1 &
+/home/ballon/reception/reception >> /home/ballon/log/reception.log 2>&1 &
 #/home/ballon/sstv/sstv >> /home/ballon/log/sstv.log 2>&1 &
