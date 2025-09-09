34cdf29355de2e0358f04e6d8ceac51839c174a2
philippeSimier <philaure@wanadoo.fr>
2025-09-09 17:47:01 +0200

    add service pour lora_files

--- 03_software_rpi0/start_ballon.sh
+++ 03_software_rpi0/start_ballon.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 sleep 30
 /home/ballon/stop/stop >> /home/ballon/log/stop.log 2>&1 &
-sleep 1
-/home/ballon/lora_files/lora_files >> /home/ballon/log/lora.log 2>&1 &
 /home/ballon/weather/weather >> /home/ballon/log/weather.log 2>&1 &
 /home/ballon/reception/reception >> /home/ballon/log/reception.log 2>&1 &
-/home/ballon/sstv/sstv >> /home/ballon/log/sstv.log 2>/dev/null
+/home/ballon/telemetrie/telemetrie >> /home/ballon/log/telemetrie.log 2>&1 &
+#/home/ballon/sstv/sstv >> /home/ballon/log/sstv.log 2>/dev/null
